a68e130210575e82f0b028f6ba5139d3a5f16c51
Jack Powell <jack@jpowell.me>
2018-03-17 18:12:38 +0000

    basic blog index layout sorted

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -14,7 +14,7 @@ class PostsController extends Controller
      */
     public function index()
     {
-        $posts = Post::orderBy('created_at')->get();
+        $posts = Post::orderBy('created_at', 'desc')->get();
 
         return view('posts.index', compact('posts'));
     }
